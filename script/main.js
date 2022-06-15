be19bc4356322177c06171a168775ce3fc98e08e
DanCan30 <danielecan3008@gmail.com>
2022-06-15 13:41:57 +0200

    Script changes

--- script/main.js
+++ script/main.js
@@ -7,6 +7,8 @@ document.getElementById("start-button").onclick = function mainFunction() {
 
     const ticketPriceOnKM = 0.21;   //Setting the standard price per KM
 
+    let ticketPrice
+
         // Setting the price for kid
     const kidDiscount = (ticketPriceOnKM * 20) / 100;
     const ticketForKid = ticketPriceOnKM - kidDiscount;
@@ -15,46 +17,37 @@ document.getElementById("start-button").onclick = function mainFunction() {
     const seniorDiscount = (ticketPriceOnKM * 40) / 100;
     const ticketForSenior = ticketPriceOnKM - seniorDiscount;
 
-    let ticketPrice
 
     if (userAge > 110) {
         document.getElementById("info-span").innerHTML = "Congratulations, You should be dead.";
     }
 
-        // Verify if the user write the correct infos (number variable type)
-    if ( isNaN(userDistance) == true ) {
-        document.getElementById("info-span").innerHTML = "Please insert a valid mileage. Reload the page.";
-        console.log("Please insert a valid mileage. Reload the page.");
-
-        // Verify if the user write the correct infos (number variable type)
-    } else if ( isNaN(userAge) == true ) {
-        document.getElementById("info-span").innerHTML = "Please insert a valid number. Reload the page.";
-        console.log("Please insert a valid number. Reload the page.");
-
-    } else {
         // User under 18. Kid discount applied
-        if (userAge <= 17) {
-            ticketPrice = userDistance * ticketForKid;
-            console.log(ticketPrice.toFixed(2));
-            document.getElementById("ticket-cost").innerHTML = ticketPrice.toFixed(2) + "€";
+    if (userAge <= 17) {
+        ticketPrice = userDistance * ticketForKid;
+        console.log(ticketPrice.toFixed(2));
 
-        // User over 65. Senior discount applied
-        } else if (userAge >= 65) {
-            ticketPrice = userDistance * ticketForSenior;
-            console.log(ticketPrice.toFixed(2));
-            document.getElementById("ticket-cost").innerHTML = ticketPrice.toFixed(2) + "€";
+    // User over 65. Senior discount applied
+    } else if (userAge >= 65) {
+        ticketPrice = userDistance * ticketForSenior;
+        console.log(ticketPrice.toFixed(2));
 
-        // Normal price applied
-        } else {
-            ticketPrice = userDistance * ticketPriceOnKM;
-            console.log(ticketPrice.toFixed(2));
-            document.getElementById("ticket-cost").innerHTML = ticketPrice.toFixed(2) + "€";
-        }
-
-        // Added more condition for training (and for the meme)
-        if ( ticketPrice >= 200 && (userAge >= 18 && userAge < 64) ) {
-            document.getElementById("info-span").innerHTML = "Better to walk.";
-        }
+    // Normal price applied
+    } else {
+        ticketPrice = userDistance * ticketPriceOnKM;
+        console.log(ticketPrice.toFixed(2));
     }
 
+    // Added more condition for training (and for the meme)
+    if ( ticketPrice >= 200 && (userAge >= 18 && userAge < 64) ) {
+        document.getElementById("info-span").innerHTML = "Better to walk.";
+    }
+    
+
+    if ( isNaN(ticketPrice) == false && ticketPrice > 0 ) {
+    document.getElementById("ticket-cost").innerHTML = ticketPrice.toFixed(2) + "€";
+    } else {
+        document.getElementById("info-span").innerHTML = "Please insert a valid number. Reload the page.";
+        console.log("Please insert a valid number. Reload the page.");
+    }
 }
